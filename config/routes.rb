92afd6a88d818e5d77d546d55e4da192092b1359
Venga Elan <vengaelan@gmail.com>
2021-11-13 14:36:58 +0800

    added CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'tasks/index'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # See all tasks
+  # get 'tasks', to: 'tasks#index'
+
+  # # Create a task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # # See details about one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Destroy a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
